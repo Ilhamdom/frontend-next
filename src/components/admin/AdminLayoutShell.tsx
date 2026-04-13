16499d1c25f7e7a3cbb6231eb16e33fbbef02748
unknown <daffamaajid@gmail.com>
2026-04-13 12:00:40 +0700

    Fixed Scroll Menu utama Left Side Only

--- src/components/admin/AdminLayoutShell.tsx
+++ src/components/admin/AdminLayoutShell.tsx
@@ -5,18 +5,26 @@ import AdminTopbar from "./AdminTopbar";
 import { SearchProvider } from "../../context/SearchContext";
 
 export default function AdminLayoutShell({ children }: { children: React.ReactNode }) {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true); // true = expanded, false = collapsed
+  // Lebar sidebar: expanded 248px (w-64), collapsed 80px (w-20)
+  const sidebarWidth = isSidebarOpen ? 248 : 80;
   return (
     <SearchProvider>
-      <div className="h-screen flex">
-        {/* Sidebar: fixed di kiri */}
-        <div className="fixed inset-y-0 left-0 z-30">
+      <div className="h-screen overflow-hidden bg-[#eef2f6]">
+        {/* Sidebar fixed dengan width dinamis */}
+        <div
+          className="fixed inset-y-0 left-0 z-30 transition-all duration-300"
+          style={{ width: sidebarWidth }}
+        >
           <AdminSidebar isOpen={isSidebarOpen} />
         </div>
-        {/* Main content: margin-left sesuai lebar sidebar */}
-        <div className="flex-1 flex flex-col h-screen ml-[248px]">
+        {/* Main content offset dinamis */}
+        <div
+          className="h-screen flex flex-col transition-all duration-300"
+          style={{ marginLeft: sidebarWidth }}
+        >
           <AdminTopbar onToggleSidebar={() => setIsSidebarOpen(prev => !prev)} />
-          <main className="flex-1 overflow-y-auto p-6 lg:p-8 bg-[#eef2f6]">
+          <main className="flex-1 overflow-y-auto p-6 lg:p-8">
             {children}
           </main>
         </div>
